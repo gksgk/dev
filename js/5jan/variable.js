9e12c58a1cdb5143fe086e1d3a6c5543f3c0cb88
gksgk <gauravndlucky@gmail.com>
2022-01-13 12:27:13 +0530

    7thjan

---
+++ js/5jan/variable.js
@@ -0,0 +1,45 @@
+//variable declaration
+
+//three types var, let, const
+
+//var
+
+var a;//initilisation of variable
+console.log(a)//will print undefined
+
+a=3
+console.log(a)//prints 3
+
+a=true
+console.log(a)//prints null
+
+a="i am a string"
+console.log(a)//prints the string
+
+a=null
+console.log(a)//prints null
+
+//this proves js is dynamically typed language
+//this is the reason we do not use data type before declaration
+
+//data types in js
+// number string boolean null undefined
+
+//Number
+var a=2
+var b=2.3
+var c=20.73838
+
+//string
+var s='a'
+var s2='gaurav kumar'
+
+//boolean
+var t=true
+var f=false
+
+//null
+var n=null
+
+//undefined
+var x
